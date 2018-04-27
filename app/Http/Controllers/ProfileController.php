3a5fff8eff5ca9912f3800b35bc5cb34d0c420f9
patel <cp425@njit.edu>
2018-04-27 01:40:23 -0400

    Editing Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Profile;
+use App\User;
 
 class ProfileController extends Controller
 {
@@ -25,9 +26,7 @@ class ProfileController extends Controller
      */
     public function create()
     {
-        $profile = new Profile();
-        $edit = FALSE;
-        return view('profileForm', ['profile' => $profile,'edit' => $edit  ]);
+        //
     }
 
     /**
@@ -38,26 +37,8 @@ class ProfileController extends Controller
      */
     public function store(Request $request)
     {
-        $input = $request->validate([
-            'fname' => 'required',
-            'lname' => 'required',
-            'body' => 'required',
-        ], [
-
-            'fname.required' => ' First is required',
-            'lname.required' => ' Last is required',
-            'body.required' => ' Body is required',
-        ]);
-        $input = request()->all();
-
-        $profile = new Profile($input);
-        $profile->user()->associate(Auth::user());
-        $profile->save();
-
-        return redirect()->route('home')->with('message', 'Profile Created');
-
+        //
     }
-
 
     /**
      * Display the specified resource.
@@ -69,7 +50,7 @@ class ProfileController extends Controller
     {
         $user = Auth::user();
         $profile = $user->profile;
-        return view( 'profile')->with('profile', $profile);
+        return view('profile')->with('profile', $profile);
     }
 
     /**
@@ -78,11 +59,14 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($user, $profile)
     {
-        //
-    }
+        $user = User::find($user);
+        $profile = $user->profile;
+        $edit = TRUE;
 
+        return view('profileForm', ['profile' => $profile, 'edit' => $edit ]);
+    }
     /**
      * Update the specified resource in storage.
      *
@@ -90,9 +74,22 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,  $user, $profile)
     {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+        ], [
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+        ]);
+        $profile = Profile::find($profile);
+        $profile->fname = $request->lname;
+        $profile->lname = $request->lname;
+        $profile->body = $request->body;
+        $profile->save();
+
+        return redirect()->route('home')->with('message', 'Updated Profile');
     }
 
     /**
